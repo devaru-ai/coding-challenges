83e34f86def485291fc1de9c7bcf3c0fb1c92349
Arundhathi Dev <devaru.ai@gmail.com>
2025-08-21 12:13:52 -0400

    Update longest_increasing_subsequence.cpp

--- leetcode/blind-75/cpp-solutions/longest_increasing_subsequence.cpp
+++ leetcode/blind-75/cpp-solutions/longest_increasing_subsequence.cpp
@@ -4,7 +4,7 @@ public:
     int n = nums.size();
     vector<int> dp(n, 1);
     for (int i = 1; i < n; ++i) {
-      for ( int j = 0; j < 1; ++j) {
+      for ( int j = 0; j < i; ++j) {
         if(nums[i] > nums[j])
           dp[i] = max(dp[i], dp[j] + 1);
       }
